a20f9a3a249573c65ba915e37cbe9000b025eabd
Pedro Dias <pedromndias@gmail.com>
2024-04-02 23:45:49 +0200

    created components

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
-import { Button, Container, Form, Grid, Header, Icon, Segment, Statistic } from "semantic-ui-react"
+import { Container, Grid, Icon, Segment } from "semantic-ui-react"
 import './App.css';
+import MainHeader from "./components/MainHeader";
+import NewEntryForm from "./components/NewEntryForm";
+import DisplayBalance from "./components/DisplayBalance";
+import DisplayBalances from "./components/DisplayBalances";
 
 function App() {
   return (
     <div className="App">
       <Container>
-        <Header as="h1">Budget</Header>
+        <MainHeader title="Budget"/>
 
-        <Statistic size="small">
-          <Statistic.Label>Your Balance:</Statistic.Label>
-          <Statistic.Value>2,550.53</Statistic.Value>
-        </Statistic>
+        <DisplayBalance title="Your Balance:" value="2,550.53" size="small" />
 
-        <Segment textAlign="center">
-          <Grid columns={2} divided>
-            <Grid.Row>
-              <Grid.Column>
-                <Statistic size="tiny" color="green">
-                  <Statistic.Label style={{textAlign:"left"}}>
-                    Income:
-                  </Statistic.Label>
-                  <Statistic.Value>1,045.50</Statistic.Value>
-                </Statistic>
-              </Grid.Column>
-              <Grid.Column>
-              <Statistic size="tiny" color="red">
-                  <Statistic.Label style={{textAlign:"left"}}>
-                    Expenses:
-                  </Statistic.Label>
-                  <Statistic.Value>623.50</Statistic.Value>
-                </Statistic>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
+        <DisplayBalances />
 
-        <Header as="h3">History</Header>
+        <MainHeader type="h3" title="History"/>
 
         <Segment color="red">
           <Grid columns={3} textAlign="right">
@@ -49,7 +29,7 @@ function App() {
             </Grid.Row>
           </Grid>
         </Segment>
-        <Segment color="red">
+        <Segment color="green">
           <Grid columns={3} textAlign="right">
             <Grid.Row>
               <Grid.Column width={10} textAlign="left">Something Else</Grid.Column>
@@ -74,18 +54,8 @@ function App() {
           </Grid>
         </Segment>
 
-        <Header as="h3">Add new Transaction</Header>
-        <Form unstackable>
-          <Form.Group>
-            <Form.Input icon="tags" width={12} label="Description" placeholder="New shinny thing" />
-            <Form.Input width={4} label="Value" placeholder="100.00"  icon="dollar" iconPosition="left"/>
-          </Form.Group>
-          <Button.Group style={{marginTop: 20}}>
-            <Button>Cancel</Button>
-            <Button.Or />
-            <Button primary>Ok</Button>
-          </Button.Group>
-        </Form>
+        <MainHeader type="h3" title="Add new Transaction" />
+        <NewEntryForm />
 
       </Container>
     </div>
